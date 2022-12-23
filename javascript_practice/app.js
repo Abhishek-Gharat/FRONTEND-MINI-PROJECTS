48db30913bfecea020975bb844bb3aa93fc09f98
Abhishek-Gharat <www.gharatabhi50@gmail.com>
2022-12-23 23:23:19 +0530

    if else,switch statements

---
+++ javascript_practice/app.js
@@ -0,0 +1,42 @@
+  // let tempInDegree =15;
+  // if(tempInDegree <0){
+  //   console.log("extremly cold outside");
+  // }
+  // else if(tempInDegree<13){
+  //   console.log("cold outside");
+  // }
+  // else if(tempInDegree<25){
+  //   console.log("weather is okey");
+  // }
+  // else if(tempInDegree<35){
+  //   console.log("lets go for swim");
+  // }
+  // else if(tempInDegree <45){
+  //   console.log("turn on Ac");
+  // }
+  // else
+  // {
+  //   console.log("too hot!!");
+  // }
+
+
+
+  // switch statement
+  let day=2;
+  switch(day){
+    case 0:
+      console.log("sunday");
+      break;
+      case 1:
+        console.log("monday");
+        break;
+        case 2:
+          console.log("tuesday");
+          break;
+          case 3:
+            console.log("wednesday");
+            break;
+            case 4:
+              console.log("Thrusday");
+              break;
+  }
