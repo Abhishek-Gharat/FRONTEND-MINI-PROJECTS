63cedd2e392d234a37af4f9f409ce658dd037a2f
Abhishek-Gharat <www.gharatabhi50@gmail.com>
2022-12-24 23:42:40 +0530

    day3

--- javascript_practice/app.js
+++ javascript_practice/app.js
@@ -22,21 +22,65 @@
 
 
   // switch statement
-  let day=2;
-  switch(day){
-    case 0:
-      console.log("sunday");
-      break;
-      case 1:
-        console.log("monday");
-        break;
-        case 2:
-          console.log("tuesday");
-          break;
-          case 3:
-            console.log("wednesday");
-            break;
-            case 4:
-              console.log("Thrusday");
-              break;
-  }
+  // let day=2;
+  // switch(day){
+  //   case 0:
+  //     console.log("sunday");
+  //     break;
+  //     case 1:
+  //       console.log("monday");
+  //       break;
+  //       case 2:
+  //         console.log("tuesday");
+  //         break;
+  //         case 3:
+  //           console.log("wednesday");
+  //           break;
+  //           case 4:
+  //             console.log("Thrusday");
+  //             break;
+  // }
+
+
+  // Array
+  let fruits=["apple","mango","graps"]
+  console.log(fruits);
+
+  // push
+  // fruits.push("banana");
+  // console.log(fruits);
+
+
+  // pop
+  fruits.pop();
+  console.log(fruits);
+
+  // unshift
+  fruits.unshift("banana");
+  fruits.unshift("myfruits")
+  console.log(fruits)
+
+  // shift
+  let removeFruit=fruits.shift();
+  console.log(fruits);
+  console.log("remove fruits is ",removeFruit)
+  // primitive vs refference data types
+
+// peimitive
+  let num1 =6;
+  let num2= num1;
+  console.log("value of num1 is",num1);
+  console.log("value of num2",num2);
+  num1++;
+// reffernce
+// array
+let array1=["item1","item2"];
+let array2=array1;
+console.log("array1",array1);
+console.log("array2",array2);
+array1.push("item3");
+console.log("after pushing item 3 in array1");
+console.log("array1",array1);
+console.log("array2",array2);
+
+
